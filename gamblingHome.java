9531293d444e59f13e7ae0dfab5fee26c8b6b6e6
MichaelKLM <102918277+MichaelKLM@users.noreply.github.com>
2022-04-03 11:52:48 -0500

    Add files via upload

---
+++ gamblingHome.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class gamblingHome {
+    public static void main(String[] args) {
+        //printHome();
+        //Await user choice
+        //Once user has chosen the specific game, call upon the code of that specific game.
+        /*
+        Essentially this is a bunch of if statements in a while statement that check to see if the input
+        matches the name of one of the given games we have created.
+         */
+
+    }
+    /*
+    printHome() {
+    printHome will be a function that prints out an easily adjustable window. This window
+    will serve as a home page in our program, from which the user can type which game they
+    would like to play (out of the ones listed on screen.)
+    for (loop for columns) {
+        for (loops for size of window) {
+            print out spaces + text
+        }
+    }
+    It may be beneficial to have a string array for this to store the names of
+     */
+
+    /*
+    blackjack() {
+    blackjack is, as the name implies, the card game blackjack. The user will be presented with the current sum of their
+    cards, and if the sum goes past 21, they lose. If they hit
+    */
+}
