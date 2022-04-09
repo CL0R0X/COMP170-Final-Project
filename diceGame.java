13cf42a4190377feae45327e8d1592b094295417
MichaelKLM <102918277+MichaelKLM@users.noreply.github.com>
2022-04-09 15:42:52 -0500

    Add files via upload

--- diceGame.java
+++ diceGame.java
@@ -13,39 +13,51 @@ public class diceGame {
     }
     public static void dice(Scanner scan) {
         System.out.println("Welcome to Dice! ");
+        System.out.println();
 
         Random roll = new Random();
         System.out.print("How many dice will you roll? (1-3)");
         int userInput = 0;
         //The Testing Portion- Making sure the input is always correct.
         //The first while statement confirms it is an integer.
-        while(!scan.hasNextInt() /*|| scan.nextInt() >3*/ ) {
+        while(!scan.hasNextInt()) {
+            scan.next();
             System.out.println("That is not a proper input, 1-3 please. ");
             System.out.print("How many dice will you roll? (1-3) ");
-            scan.next();
         }
         //The second uses a placeholder variable to ensure the integer that userInput
         //will be is within the proper range.
 
-        // I need help working out this portion.
-        int tester = scan.nextInt();
-        if (tester > 0 && tester < 4) {
-            userInput = tester;
-        } else {
-            while (tester <= 0 && tester >= 4) {
-                System.out.println("That is not a proper input, 1-3 please. ");
-                //scan.next();
-                tester = scan.nextInt();
+
+        // I GOT THIS TO WORK
+        userInput = scan.nextInt();
+        System.out.println(userInput);
+        int brakeCheck = 0;
+        while (brakeCheck == 0) {
+            System.out.print("That is not a proper input, 1-3 please. ");
+            while(!scan.hasNextInt()) {
+                scan.next();
+                System.out.print("That is not a proper input, 1-3 please. ");
+            }
+            userInput = scan.nextInt();
+            if (userInput > 0 && userInput < 4) {
+                brakeCheck = 1;
             }
         }
+
+        System.out.println(userInput);
+        // User's guess on what it lands on.
         System.out.print("What do you think it will land on? ");
         int guess = scan.nextInt();
 
         //time to start rolling!
         int diceRollResult = 0;
-        System.out.println("You chose " + userInput + " die to roll, \n which means you'll have " + (diceSides * userInput) + " 3different outputs." );
+        System.out.println("You chose " + userInput + " die to roll, which means \nyou'll have " + (diceSides * userInput) + " different outputs." );
         System.out.println("Its time to roll!");
-        diceRollResult = roll.nextInt((diceSides * userInput) -1) + 1;
+
+        System.out.println();// (spacer)
+        // Rolls the dice!
+        diceRollResult = roll.nextInt((diceSides * userInput) - 1) + 1;
         System.out.println("Your result = " + diceRollResult);
         if (diceRollResult == guess && userInput == 1) {
             System.out.println("You win!");
