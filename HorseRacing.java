606dfed8f6fb55474c4a1b0f468337efe74572ff
CL0R0X <94938596+CL0R0X@users.noreply.github.com>
2022-04-13 22:37:02 -0500

    Add files via upload

---
+++ HorseRacing.java
@@ -0,0 +1,41 @@
+public class HorseRacing {
+    /*
+     *  First have the console print out information about how Horse race willl work
+     *  let the user pick/determine the amount of money they want to bet
+     *
+     *
+     *  Have the console next print out 4 different track:
+     * --------------------
+     * track 1
+     * --------------------
+     * track 2
+     * --------------------
+     * track 3
+     * -------------------
+     * track 4
+     *
+     * have those 4 tracks listed in console
+     * let the user pick which of the 4 tracks to bet on
+     *
+     * after the user picks which track he wants to bet on
+     * save that into a variable
+     * next send that variable into another method
+     *
+     * in order for the tracks to move to the end
+     * create a random int with max of 1, there can be only 2 options 0 or 1
+     * if the random integer hits a 1 that object would switch places with -
+     * do a while loop for the track to happen
+     * the first track that reaches to " "
+     * record that track and send it to the other method
+     * about which track the user had beted on
+     *
+     * that method will receive 2 different values
+     * which determines if the user wins or loses
+     * using a if else statements
+     *
+     *
+     * */
+    public static void main(String[] args) {
+
+    }
+}
