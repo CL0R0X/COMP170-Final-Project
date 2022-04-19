5e85edbe88122d806ec756a4d1f08b5d9160c387
CL0R0X <94938596+CL0R0X@users.noreply.github.com>
2022-04-19 17:19:08 -0500

    Update HorseRacing.java

--- HorseRacing.java
+++ HorseRacing.java
@@ -1,3 +1,7 @@
+
+import java.util.Random;
+
+
 public class HorseRacing {
     /*
      *  First have the console print out information about how Horse race willl work
@@ -35,7 +39,65 @@ public class HorseRacing {
      *
      *
      * */
+   public static final int horseSum = 5;
+    public static final int trackLength = 10;
     public static void main(String[] args) {
+        System.out.println("Welcome to the Horse Race!\n");
+        //setting up the String array for the track.
+        Random rand = new Random();
 
+        String[] track = new String[horseSum];
+        int[] horsePlaces =  new int[horseSum];
+        //This has to be a separate block of code from the other prints for now.
+        for (int j = 0; j < horseSum; j++) {
+            track[j] = "O";
+            for (int i = 0; i < trackLength - 1; i++) {
+                track[j] += "-";
+            }
+            System.out.println("Horse #" + (j + 1) + ": " + track[j]);
+        }
+        // Time to run the race!
+        int weHaveAWinner = 0;
+        int winnerPlace = 0;
+        while (weHaveAWinner == 0) {
+            printTrack(track);
+            int boostPlace = rand.nextInt(horseSum);
+            track = horseForward(boostPlace, horsePlaces, track);
+            horsePlaces[boostPlace]++;
+            if (horsePlaces[boostPlace] == trackLength) {
+                weHaveAWinner = 1;
+                winnerPlace = boostPlace;
+            }
+            System.out.println();
+        }
+
+        System.out.println("The Winner is Horse #" + (winnerPlace + 1) + "!!!" );
+
+
+
+    }
+    public static String[] horseForward(int place, int[] horsePlaces, String[] raceTrack) {
+        /*
+        for (int j = 0; j < horseSum; j++) {
+            //track[j] = "O";
+            for (int i = 0; i < trackLength - 1; i++) {
+                //track[j] += "-";
+            }
+            System.out.println("Horse #" + (j + 1) + ": " + track[j]);
+        } */
+        // for ease of typing
+        String returning = raceTrack[place];
+        String line = "-";
+
+        returning = returning.substring(0, returning.length() - 1);
+        returning = line + returning;
+        raceTrack[place] = returning;
+        return raceTrack;
+    }
+
+    public static void printTrack(String[] track) {
+        for (int i = 0; i < horseSum; i++) {
+            System.out.println("Horse #" + (i + 1) + ": " + track[i]);
+        }
     }
 }
