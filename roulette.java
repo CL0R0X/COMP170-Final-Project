6999f63cbf25c5228fdc3265d07fc065214d69ff
CL0R0X <94938596+CL0R0X@users.noreply.github.com>
2022-04-22 13:06:34 -0500

    Update roulette.java

--- roulette.java
+++ roulette.java
@@ -3,7 +3,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class roulette {
-    public static void main(String[]  args) {
+    public static void games() {
         Random rand = new Random();
         Scanner scan = new Scanner(System.in);
         System.out.print("Would you like to bet on Color or Number or Both (0/1/2): ");
