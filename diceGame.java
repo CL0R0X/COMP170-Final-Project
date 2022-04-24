e519d1e9b3adf8866dc5d014e44bc2284ea11b5f
MichaelKLM <102918277+MichaelKLM@users.noreply.github.com>
2022-04-24 11:40:24 -0500

    Update diceGame.java

--- diceGame.java
+++ diceGame.java
@@ -5,13 +5,15 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class diceGame {
+
     // Amount of "sides" for the dice.
+
     public static final int diceSides = 6;
-    public static void games() {
+    public static void games(coin userBalance) {
         Scanner console = new Scanner(System.in);
-        dice(console);
+        dice(console, userBalance);
     }
-    public static void dice(Scanner scan) {
+    public static void dice(Scanner scan, coin userBalance) {
         System.out.println("Welcome to Dice! ");
         System.out.println();
 
@@ -28,8 +30,6 @@ public class diceGame {
         //The second uses a placeholder variable to ensure the integer that userInput
         //will be is within the proper range.
 
-
-        // I GOT THIS TO WORK
         userInput = scan.nextInt();
         int brakeCheck = 0;
         if (userInput > 0 && userInput < 4) {
@@ -37,6 +37,21 @@ public class diceGame {
         }
         if (brakeCheck == 0) {
             userInput =  inputChecker(scan, 0, 4);
+        }
+
+        //Betting part of the code.
+
+        System.out.println("Please input how much you would like to bet: ");
+        while(!scan.hasNextInt()) {
+            scan.next();
+            System.out.println("That is not an integer. ");
+        }
+        int initialBet = scan.nextInt();
+
+        if (initialBet <= userBalance.getBal()) {
+            System.out.println(userBalance.getBal());
+        } else {
+            initialBet = inputChecker(scan, 1, (int) userBalance.getBal());
         }
 
 
@@ -51,31 +66,37 @@ public class diceGame {
         System.out.println("Its time to roll!");
 
         System.out.println();// (spacer)
+
         // Rolls the dice!
         diceRollResult = roll.nextInt((diceSides * userInput) - 1) + 1;
         System.out.println("Your result = " + diceRollResult);
         if (diceRollResult == guess && userInput == 1) {
             System.out.println("You win!");
+            userBalance.newBal((initialBet * 1.5));
             //win bet amount * 1.5
         } else if (diceRollResult == guess && userInput == 2) {
             System.out.println("You win!");
+            userBalance.newBal((initialBet * 2.5));
             //win bet amount * 2.5
         } else if (diceRollResult == guess && userInput == 3) {
             System.out.println("You win!");
+            userBalance.newBal((initialBet * 3.5));
             //win bet amount * 3.5
         } else {
             System.out.println("No dice!");
+            userBalance.newBal((initialBet * -1));
         }
+        System.out.println(userBalance.getBal());
     }
 
     public static int inputChecker(Scanner scan, int min, int max) {
         int brakeCheck = 0;
         int returnInput = 0;
         while (brakeCheck == 0) {
-            System.out.print("That is not a proper input, 1-3 please. ");
+            System.out.print("That is not a proper input.");
             while(!scan.hasNextInt()) {
                 scan.next();
-                System.out.print("That is not a proper input, 1-3 please. ");
+                System.out.print("That is not a proper input.");
             }
             returnInput = scan.nextInt();
             if (returnInput > min && returnInput < max) {
@@ -85,16 +106,3 @@ public class diceGame {
         return returnInput;
     }
 }
-/*
-while (brakeCheck == 0) {
-            System.out.print("That is not a proper input, 1-3 please. ");
-            while(!scan.hasNextInt()) {
-                scan.next();
-                System.out.print("That is not a proper input, 1-3 please. ");
-            }
-            userInput = scan.nextInt();
-            if (userInput > 0 && userInput < 4) {
-                brakeCheck = 1;
-            }
-        }
- */
