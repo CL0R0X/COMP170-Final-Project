f4db54e8ac9a9c7008d18cf84a633f07e86475c3
CL0R0X <94938596+CL0R0X@users.noreply.github.com>
2022-04-24 15:29:03 -0500

    Update roulette.java

--- roulette.java
+++ roulette.java
@@ -10,80 +10,127 @@ public class roulette {
         Random rand = new Random();
         Scanner scan = new Scanner(System.in);
 
-
-
-
         System.out.print("Would you like to bet on Color or Number or Both (0/1/2): ");
         int request = scan.nextInt();
 
-
+        System.out.println("Your current balance is " + userBalance.getBal() + " coins");
         int rolledScore = rand.nextInt(36);
         System.out.println(rolledScore);
         System.out.print("How many coins do you want to bet? ");
-        int coins = scan.nextInt();
 
-        if (request == 0 ) {
-            System.out.printf("What color tile do you pick red or black or green(0/1/2)? ");
-            int color = scan.nextInt();
-            //color = color.toUpperCase();
-            colorOfChoice(rolledScore, color, scan, userBalance);
-        } else if(request == 1 ) {
-            System.out.printf("What number out of 36 do you pick? ");
-            int guessNumber = scan.nextInt();
-            gambleNum(guessNumber, rolledScore);
-        } else if( request == 2) {
-            System.out.printf("What number out of 36 do you pick? ");
-            int guessNumber = scan.nextInt();
-            gambleNum(guessNumber, rolledScore);
-            System.out.printf("What color tile do you pick red or black or green(0/1/2)? ");
-            int color = scan.nextInt();
-            colorOfChoice(rolledScore, color, scan, userBalance);
-
-        }
-    }
-
-    public static void colorOfChoice( int rolledNumber, int color, Scanner scan, coin userBalance) {
-        System.out.println("Before you roll, please input how much you would like to bet: ");
         while(!scan.hasNextInt()) {
             scan.next();
             System.out.println("That is not an integer. ");
         }
-        int initialBet = scan.nextInt();
+        int coins = scan.nextInt();
 
-        if (initialBet <= userBalance.getBal()) {
+        if (coins <= userBalance.getBal()) {
             System.out.println(userBalance.getBal());
         } else {
-            initialBet = inputChecker(scan, 1, (int) userBalance.getBal());
+            coins = inputChecker(scan, 1, (int) userBalance.getBal());
         }
-
-        if (rolledNumber %2 == 0 && color == 0) {
-            System.out.println("You rolled a "+rolledNumber);
-            System.out.printf("You have won, double your coins");
-            userBalance.newBal((initialBet * 2));
-        } else if ( rolledNumber %2 == 1 && color == 1) {
-            System.out.println("You rolled a "+rolledNumber);
-            System.out.printf("You have won, double your coins");
-            userBalance.newBal((initialBet * 2));
-        }else if( (rolledNumber == 0 || rolledNumber == 36 )&& color == 2) {
-            System.out.printf("You have won, triple your coins");
-            System.out.println("You rolled a "+rolledNumber);
-            userBalance.newBal((initialBet * 3));
-        } else {
-            System.out.print("Better luck next time ");
-            System.out.println(rolledNumber);
-            userBalance.newBal((initialBet * -1));
-        }
+        gambleTypes(scan, coins, userBalance, request, rolledScore);
     }
 
-    public static void gambleNum( int guess, int rolledNumber) {
-        if(guess == rolledNumber) {
-            System.out.printf("You have guessed " + guess + " and rolled " + rolledNumber + " ");
-            System.out.println ("You won 5 times your money");
-            
-        } else {
-            System.out.printf("You have guessed " + guess + " and rolled " + rolledNumber);
-            System.out.println("Better luck Next Time");
+
+    public static void gambleTypes(Scanner scan , int initialBet, coin userBalance, int request, int rolledNumber) {
+        if (request == 0 ) {
+            System.out.printf("What color tile do you pick red or black or green(0/1/2)? ");
+            int color = scan.nextInt();
+
+            if (rolledNumber %2 == 0 && color == 0) {
+                System.out.println("You rolled a "+rolledNumber);
+                System.out.printf("You have won, double your coins");
+                userBalance.newBal((initialBet * 2));
+            } else if ( rolledNumber %2 == 1 && color == 1) {
+                System.out.println("You rolled a "+rolledNumber);
+                System.out.printf("You have won, double your coins");
+                userBalance.newBal((initialBet * 2));
+            }else if( (rolledNumber == 0 || rolledNumber == 36 )&& color == 2) {
+                System.out.printf("You have won, triple your coins");
+                System.out.println("You rolled a "+rolledNumber);
+                userBalance.newBal((initialBet * 3));
+            } else {
+                System.out.print("Better luck next time ");
+                System.out.println(rolledNumber);
+                userBalance.newBal((initialBet * -1));
+            }
+            System.out.println("Your current balance is " + userBalance.getBal() + " coins");
+        } else if(request == 1) {
+            System.out.printf("What number out of 36 do you pick? ");
+            int guess = scan.nextInt();
+            //gambleNum(guessNumber, rolledScore), userBalance, coins;
+            if(guess == rolledNumber) {
+                System.out.printf("You have guessed " + guess + " and rolled " + rolledNumber + " ");
+                System.out.println ("You won 5 times your money");
+                userBalance.newBal(initialBet*5);
+            } else {
+                System.out.printf("You have guessed " + guess + " and rolled " + rolledNumber);
+                System.out.println("Better luck Next Time");
+                userBalance.newBal(initialBet *-1);
+            }
+            System.out.println("Your current balance is " + userBalance.getBal() + " coins");
+
+        } else if( request == 2) {
+            System.out.printf("What number out of 36 do you pick? ");
+            int guess = scan.nextInt();
+            System.out.printf("What color tile do you pick red or black or green(0/1/2)? ");
+            int color = scan.nextInt();
+            //gambleNum(guessNumber, rolledScore), userBalance, coins;
+
+            //colorOfChoice(rolledScore, color, scan, userBalance, coins);
+            if (rolledNumber %2 == 0 && color == 0) {
+                //red
+                System.out.println("You rolled a "+rolledNumber + " and your color tile is red");
+                if(guess == rolledNumber ) {
+                    System.out.printf("You have guessed " + guess + " and rolled " + rolledNumber + " ");
+                    System.out.println ("You won 5 times your money");
+                    userBalance.newBal(initialBet*5);
+                } else {
+                    System.out.printf("You have guessed " + guess + " and rolled " + rolledNumber);
+                    System.out.println("Better luck Next Time");
+                    userBalance.newBal(initialBet *-1);
+                }
+            } else if ( rolledNumber %2 == 1 && color == 1) {
+                //black
+                System.out.println("You rolled a "+rolledNumber + " and your color tile is black");
+                if(guess == rolledNumber ) {
+                    System.out.printf("You have guessed " + guess + " and rolled " + rolledNumber + " ");
+                    System.out.println ("You won 5 times your money");
+                    userBalance.newBal(initialBet*5);
+                } else {
+                    System.out.printf("You have guessed " + guess + " and rolled " + rolledNumber);
+                    System.out.println("Better luck Next Time");
+                    userBalance.newBal(initialBet *-1);
+                }
+            }else if( (rolledNumber == 0 || rolledNumber == 36 )&& color == 2) {
+               //green
+                System.out.println("Your color tile is green");
+                if(guess == rolledNumber ) {
+                    System.out.printf("You have guessed " + guess + " and rolled " + rolledNumber + " ");
+                    System.out.println ("You won 6 times your money");
+                    userBalance.newBal(initialBet*6);
+                } else {
+                    System.out.printf("You have guessed " + guess + " and rolled " + rolledNumber);
+                    System.out.println("Better luck Next Time");
+                    userBalance.newBal(initialBet *-1);
+                }
+            } else {
+                System.out.print("Better luck next time ");
+                if( (rolledNumber == 0 || rolledNumber == 36 )) {
+                    System.out.println("You landed on " + rolledNumber + " and a green tile");
+                } else if( rolledNumber %2 == 1 ) {
+                    System.out.println("You landed on " + rolledNumber + " and a black tile");
+                } else {
+                    System.out.println("You landed on " + rolledNumber + " and a red tile");
+                }
+                userBalance.newBal((initialBet * -1));
+            }
+            System.out.println("Your current balance is " + userBalance.getBal() + " coins");
+            playAgain(userBalance);
         }
+
+
     }
     public static int inputChecker(Scanner scan, int min, int max) {
         int brakeCheck = 0;
@@ -102,7 +149,9 @@ public class roulette {
         return returnInput;
     }
 
-
+    public static void playAgain (coin userBalance) {
+        gamblingHome.whichGames(userBalance);
+    }
 }
 
 
