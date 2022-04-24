906902fcc1b187ab09fa7fb5df388875d7336b01
MichaelKLM <102918277+MichaelKLM@users.noreply.github.com>
2022-04-24 12:03:30 -0500

    Update roulette.java

--- roulette.java
+++ roulette.java
@@ -1,11 +1,18 @@
+package com.company;
+
 import java.util.Locale;
 import java.util.Random;
 import java.util.Scanner;
 
 public class roulette {
-    public static void games() {
+    public static void games(coin userBalance) {
+        System.out.println("Welcome to Roulette! ");
         Random rand = new Random();
         Scanner scan = new Scanner(System.in);
+
+
+
+
         System.out.print("Would you like to bet on Color or Number or Both (0/1/2): ");
         int request = scan.nextInt();
 
@@ -19,7 +26,7 @@ public class roulette {
             System.out.printf("What color tile do you pick red or black or green(0/1/2)? ");
             int color = scan.nextInt();
             //color = color.toUpperCase();
-            colorOfChoice(rolledScore, color);
+            colorOfChoice(rolledScore, color, scan, userBalance);
         } else if(request == 1 ) {
             System.out.printf("What number out of 36 do you pick? ");
             int guessNumber = scan.nextInt();
@@ -30,27 +37,41 @@ public class roulette {
             gambleNum(guessNumber, rolledScore);
             System.out.printf("What color tile do you pick red or black or green(0/1/2)? ");
             int color = scan.nextInt();
-            colorOfChoice(rolledScore, color);
+            colorOfChoice(rolledScore, color, scan, userBalance);
 
         }
     }
 
-    public static void colorOfChoice( int rolledNumber, int color) {
+    public static void colorOfChoice( int rolledNumber, int color, Scanner scan, coin userBalance) {
+        System.out.println("Before you roll, please input how much you would like to bet: ");
+        while(!scan.hasNextInt()) {
+            scan.next();
+            System.out.println("That is not an integer. ");
+        }
+        int initialBet = scan.nextInt();
+
+        if (initialBet <= userBalance.getBal()) {
+            System.out.println(userBalance.getBal());
+        } else {
+            initialBet = inputChecker(scan, 1, (int) userBalance.getBal());
+        }
+
         if (rolledNumber %2 == 0 && color == 0) {
             System.out.println("You rolled a "+rolledNumber);
             System.out.printf("You have won, double your coins");
-            
+            userBalance.newBal((initialBet * 2));
         } else if ( rolledNumber %2 == 1 && color == 1) {
             System.out.println("You rolled a "+rolledNumber);
             System.out.printf("You have won, double your coins");
-            
+            userBalance.newBal((initialBet * 2));
         }else if( (rolledNumber == 0 || rolledNumber == 36 )&& color == 2) {
             System.out.printf("You have won, triple your coins");
             System.out.println("You rolled a "+rolledNumber);
-            
+            userBalance.newBal((initialBet * 3));
         } else {
             System.out.print("Better luck next time ");
             System.out.println(rolledNumber);
+            userBalance.newBal((initialBet * -1));
         }
     }
 
@@ -64,7 +85,26 @@ public class roulette {
             System.out.println("Better luck Next Time");
         }
     }
+    public static int inputChecker(Scanner scan, int min, int max) {
+        int brakeCheck = 0;
+        int returnInput = 0;
+        while (brakeCheck == 0) {
+            System.out.print("That is not a proper input.");
+            while(!scan.hasNextInt()) {
+                scan.next();
+                System.out.print("That is not a proper input.");
+            }
+            returnInput = scan.nextInt();
+            if (returnInput > min && returnInput < max) {
+                brakeCheck = 1;
+            }
+        }
+        return returnInput;
+    }
 
-    
 
 }
+
+
+
+
